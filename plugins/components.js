a1d115e650bb90f82991b9b85eaea9bee9bb39c7
Andrew Boyd <andrew@wearebraid.com>
2018-08-13 14:52:54 -0400

    Clean up! Clean up! Everybody everywhere! Clean up! Clean up! Everybody do your share!

--- plugins/components.js
+++ plugins/components.js
@@ -4,11 +4,9 @@ import Vue from 'vue'
  * Import components
  */
 import pageHero from '~/components/pageHero'
-import beSponsor from '~/components/howtobesponsor'
 
 /**
  * Register components.
  */
 Vue.component('page-hero', pageHero)
-Vue.component('be-sponsor', beSponsor)
 

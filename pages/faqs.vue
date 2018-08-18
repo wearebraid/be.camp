e51baf0440a076a1de54f3fe5b917678b897ec9c
Andrew Boyd <andrew@wearebraid.com>
2018-08-18 14:28:43 -0400

    add FAQs page with content from butterCMS

---
+++ pages/faqs.vue
@@ -0,0 +1,53 @@
+<template>
+  <div
+    v-if="page"
+    class="page-wrapper"
+  >
+    <page-hero
+      :background="page.hero_image"
+      :accent-color="page.page_accent_color"
+    >
+      <h1 class="page-title">
+        {{ page.page_title }}
+      </h1>
+      <div class="page-description">
+        {{ page.page_description }}
+      </div>
+    </page-hero>
+
+    <section class="page-section">
+      <div class="wysiwyg-block">
+        <div v-html="page.page_content"></div>
+      </div>
+    </section>
+  </div>
+</template>
+
+<script>
+import {mapState} from 'vuex'
+
+export default {
+  async fetch ({store, params}) {
+    try {
+      await store.dispatch('getPage', 'faqs')
+    } catch(err) {
+      console.log(err)
+    }
+  },
+  created () {
+    this.$store.commit('setCurrentPageAccentColor', this.page.page_accent_color)
+  },
+  computed: {
+    ...mapState({
+      butterPages: state => state.butterPages,
+    }),
+    page () {
+      return this.butterPages['faqs'] ? this.butterPages['faqs'] : {}
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>

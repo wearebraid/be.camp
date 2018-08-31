6bc97b8955aed7c0a62e3c0f29edc516412e1b12
Andrew Boyd <andrew@wearebraid.com>
2018-08-31 15:01:02 -0400

    add support for linking to page hashes

--- nuxt.config.js
+++ nuxt.config.js
@@ -77,6 +77,24 @@ module.exports = {
       sendHitTask: isProd
     }
   },
+  router: {
+    scrollBehavior(to, from, savedPosition) {
+      if (savedPosition) {
+        return savedPosition
+      } else {
+        let position = {}
+        if (to.matched.length < 2) {
+          position = { x: 0, y: 0 }
+        } else if (to.matched.some(r => r.components.default.options.scrollToTop)) {
+          position = { x: 0, y: 0 }
+        }
+        if (to.hash) {
+          position = { selector: to.hash }
+        }
+        return position
+      }
+    }
+  },
   /*
   ** Build configuration
   */

338e8a9f822aa9fa153cb5aed391e9dd253df47c
Andrew Boyd <andrew@wearebraid.com>
2018-09-22 11:59:40 -0400

    load google fonts with JS to improve initial render performance

--- nuxt.config.js
+++ nuxt.config.js
@@ -28,8 +28,7 @@ module.exports = {
       { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/favicon-32x32.png' },
       { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/favicon-16x16.png' },
       { rel: 'manifest', href: '/site.webmanifest' },
-      { rel: 'mask-icon', href: '/safari-pinned-tab.svg', color: '#e67711' },
-      { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Open+Sans:700' }
+      { rel: 'mask-icon', href: '/safari-pinned-tab.svg', color: '#e67711' }
     ],
     script: [
       { src: '/youtube.js', async: true, rel: 'preload' },
@@ -43,6 +42,7 @@ module.exports = {
   plugins: [
     '~/plugins/components',
     '~/plugins/api',
+    {src: '~/plugins/webFontLoader', ssr: false},
     {src: '~/plugins/lazyload', ssr: false},
     {src: '~/plugins/events', ssr: false},
     {src: '~/plugins/localStorage.js', ssr: false}

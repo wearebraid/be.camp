bbbe4b4b16bb899c101f5543b5bc856d6694bd4a
Dylan Sheffer <dsheffer16@gmail.com>
2019-04-04 21:16:11 -0400

    add larger favicons to improve PWA experience

--- nuxt.config.js
+++ nuxt.config.js
@@ -33,6 +33,8 @@ module.exports = {
       { rel: 'apple-touch-icon', sizes: '152x152', href: '/apple-touch-icon.png' },
       { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/favicon-32x32.png' },
       { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/favicon-16x16.png' },
+      { rel: 'icon', type: 'image/png', sizes: '192x192', href: '/favicon-192x192.png' },
+      { rel: 'icon', type: 'image/png', sizes: '512x512', href: '/favicon-512x512.png' },
       { rel: 'manifest', href: '/site.webmanifest' },
       { rel: 'mask-icon', href: '/safari-pinned-tab.svg', color: '#e67711' }
     ],
